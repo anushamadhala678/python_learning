65351672a77aeb24bda71eafaebd0b97b41e710b
anusha <anushamadhala678@gmail.com>
2025-01-30 17:34:33 +0000

    operators learning

---
+++ operators/Arithmetic_operators/examples.py
@@ -0,0 +1,41 @@
+# Addition
+a = 10
+b = 5
+result = a + b
+print(result)
+
+# Substraction
+a = 10
+b = 5
+result = a - b
+print(result)
+
+# Multiplication
+a = 10
+b = 5
+result = a * b
+print(result)
+
+# Division
+a = 10
+b = 5
+result = a / b
+print(result)
+
+# Modulus
+a = 10
+b = 5
+result = a % b
+print(result)
+
+#Exponentiation
+a = 10
+b = 5
+result = a ** b   # 10^5
+print(result)
+
+# Floor division
+a = 10
+b = 5
+result = 8 // 5
+print(result)
